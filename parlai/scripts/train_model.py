ee2bec54439d8ae5f48ba949ce3d21ea5342c252
Jackberg <s.jiang@uva.nl>
2018-07-26 18:47:48 +0200

    Allow using validation set to tune parameters.

--- parlai/scripts/train_model.py
+++ parlai/scripts/train_model.py
@@ -279,7 +279,7 @@ class TrainLoop():
         world = self.world
         if opt.get('model_file'):
             self.agent = create_agent(opt)
-        rep, wrld = run_eval(self.agent, opt, 'test', write_log=True)
+        rep, wrld = run_eval(self.agent, opt, opt['datatype'], write_log=True)
         wrld.shutdown()  # may need to shut down threads, remote connections
         return rep
 
