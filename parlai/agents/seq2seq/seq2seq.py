7c10cd4f330cce0dbd9b12c604c6ce6fd09a6d45
Jackberg <s.jiang@uva.nl>
2018-08-15 22:13:55 +0200

    Entropy as weight.

--- parlai/agents/seq2seq/seq2seq.py
+++ parlai/agents/seq2seq/seq2seq.py
@@ -388,6 +388,7 @@ class Seq2seqAgent(Agent):
 
     def reset_metrics(self):
         self.metrics['loss'] = 0.0
+        self.metrics['entropy'] = 0.0
         self.metrics['num_tokens'] = 0.0
         self.metrics['1_gram'] = Counter()
         self.metrics['2_gram'] = Counter()
@@ -399,6 +400,7 @@ class Seq2seqAgent(Agent):
         m = {}
         if self.metrics['num_tokens'] > 0:
             m['loss'] = self.metrics['loss'] / self.metrics['num_tokens']
+            m['E'] = self.metrics['entropy'] / self.metrics['num_tokens']
             m['ppl'] = math.exp(m['loss'])
             m['d_1'] = len(self.metrics['1_gram']) / self.metrics['out_tokens']
             m['d_2'] = len(self.metrics['2_gram']) / self.metrics['out_tokens']
@@ -465,15 +467,17 @@ class Seq2seqAgent(Agent):
             log_scores = scores.log()
             entropy = -torch.bmm(scores.view(-1, 1, scores.size(-1)), log_scores.view(-1, scores.size(-1), 1)).view(-1)
             mask = (ys.view(-1) != self.NULL_IDX).float()
-            mask /= mask.sum()
-            mean_entropy = torch.matmul(entropy, mask) # exclude null token
+            entropy = torch.matmul(entropy, mask) # exclude null token
             loss = self.criterion(log_scores.view(-1, log_scores.size(-1)), ys.view(-1))
             # save loss to metrics
-            target_tokens = ys.ne(self.NULL_IDX).long().sum().data[0]
+            target_tokens = mask.sum().data[0]
             self.metrics['loss'] += loss.double().data[0]
             self.metrics['num_tokens'] += target_tokens
+            self.metrics['entropy'] += entropy.data[0]
             loss /= target_tokens  # average loss per token
-            loss += 1/mean_entropy
+            entropy /= target_tokens
+            weight = 1 + 1 / entropy.data[0]
+            loss *= weight
             # loss /= xs.size(0)  # average loss per sentence
             loss.backward()
             self.update_params()
